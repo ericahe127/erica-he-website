92a01887609d4edd949125ce674d8ac28ce33e10
ericahe127 -Git <hexuan426@gmail.com>
2023-12-09 22:46:19 -0800

    added constraint

--- contact.js
+++ contact.js
@@ -1,18 +1,15 @@
 $(document).ready(function () {
         $('#send-button').on('click', function () {
-
-            $('#send-button').html("Sent😁");
-            $('#send-button').removeClass("btn-outline-primary");
-            $('#send-button').addClass("btn-success");
-            setTimeout(() => {
-                $('#send-button').html("Send");
-                $('#send-button').addClass("btn-outline-primary");
-                $('#send-button').removeClass("btn-success");
-            }, 1000);
             // Get input values using jQuery
             var name = $('#nameInput').val();
             var email = $('#emailInput').val();
             var message = $('#messageInput').val();
+            
+            // Check if the mandatory fields are not empty
+            if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+                alert('Please fill in all mandatory fields.');
+                return;
+            }
 
             // Do something with the values (for now, just log them to the console)
             console.log('Name:', name);
@@ -23,5 +20,13 @@ $(document).ready(function () {
             $('#nameInput').val('');
             $('#emailInput').val('');
             $('#messageInput').val('');
+            $('#send-button').html("Sent😁");
+            $('#send-button').removeClass("btn-outline-primary");
+            $('#send-button').addClass("btn-success");
+            setTimeout(() => {
+                $('#send-button').html("Send");
+                $('#send-button').addClass("btn-outline-primary");
+                $('#send-button').removeClass("btn-success");
+            }, 1000);
         });
     });
